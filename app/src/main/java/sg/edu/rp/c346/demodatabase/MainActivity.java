e1f9fc36b6e03a0c31dc9f9ebc40f641307e6088
Herwan Shah <shahherwan@hotmail.com>
2017-05-11 12:36:00 +0800

    2.	Create a Java class from the class diagram above. Also, create the constructor and the getters methods. 3.	Create the setters and getters for the fields and also a 3 parameter constructor to initialize the fields. 4.	Create a method called getTasks() in DBHelper that returns ArrayList<Task>.

--- app/src/main/java/sg/edu/rp/c346/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demodatabase/MainActivity.java
@@ -2,12 +2,60 @@ package sg.edu.rp.c346.demodatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnGetTasks;
+    TextView tvResults;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnGetTasks = (Button) findViewById(R.id.btnGetTasks);
+        tvResults = (TextView)  findViewById(R.id.tvResults);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the activity Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<Task> data = db.getTask();
+                db.close();
+
+                String txt = "";
+                for(int i = 0; i < data.size(); i++){
+                    Log.d("Database Content", i +". " +data.get(i));
+                    txt += i + ". " +data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+
+
+            }
+        });
     }
 }
